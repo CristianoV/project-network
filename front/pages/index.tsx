f80dc6afb52f1e6724d9344be3c3e85fddbac144
CristianoV <cristianoviieira@gmail.com>
2023-03-15 17:14:50 -0300

    feat(front): adiciona esqueleto inicial do front

---
+++ front/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Rede Social</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Orkut</h1>
+      </main>
+    </>
+  )
+}
