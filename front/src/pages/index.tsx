fc9b210424b8fc1739dac8420726b8d14adec3a6
CristianoV <cristianoviieira@gmail.com>
2023-03-20 19:03:33 -0300

    feat(front): implementa busca por token de validação a pagina principal

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,28 +1,33 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      router.push('/account');
-    } else {
-      router.push('/login');
-    }
-  }, [router]);
-
   return (
     <>
       <Head>
         <title>Rede Social</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <h1>Orkut</h1>
-      </main>
+      <main></main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const token = req.cookies.token || '';
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      token,
+    },
+  };
+};
