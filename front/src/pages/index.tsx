b906bd66f5a0048f84767a67d3f1a5b0bdbf316c
CristianoV <cristianoviieira@gmail.com>
2023-03-20 14:36:05 -0300

    feat(front): adiciona novas configurações da pagina

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,15 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      router.push('/account');
+    } else {
+      router.push('/login');
+    }
+  }, [router]);
+
   return (
     <>
       <Head>
         <title>Rede Social</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
         <h1>Orkut</h1>
       </main>
     </>
-  )
+  );
 }
