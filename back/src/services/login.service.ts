ceea5c25ded30db7835698d8f69b3b5583326fa1
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-04 23:06:42 -0300

    feat(back): adiciona interfaces no service de login

--- back/src/services/login.service.ts
+++ back/src/services/login.service.ts
@@ -2,15 +2,15 @@ import JwtSecret from '../utils/JwtService';
 import User from '../database/models/user';
 import Bcrypt from '../utils/BcryptService';
 import { ILoginService } from '../interface/IService/ILoginService';
-import { IUserData, userSchema } from '../interface/IData/IUserData';
+import { loginSchema, ILoginData } from '../interface/IData/ILoginData';
 import { IToken } from '../interface/IToken';
 import { IError } from '../interface/IError';
 
 export default class LoginService implements ILoginService {
   constructor(private model: typeof User) {}
 
-  public async login({ email, password }: IUserData): Promise<IToken | IError> {
-    const parsed = userSchema.safeParse({ email, password });
+  public async login({ email, password }: ILoginData): Promise<IToken | IError> {
+    const parsed = loginSchema.safeParse({ email, password });
 
     if (!parsed.success) {
       const { message } = parsed.error;
@@ -18,12 +18,14 @@ export default class LoginService implements ILoginService {
 
       throw new Error(customMessage[0].message);
     }
-    const user = await this.model.findOne({
-      where: {
-        email,
+
+    const user = await this.model.findOne(
+      {
+        where: {
+          email,
+        },
       },
-      raw: true,
-    });
+    );
 
     if (!user) {
       throw new Error('User not found');
