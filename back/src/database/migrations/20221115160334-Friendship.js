4b1055bb22c47f917bd263c4a05bb98d52dfc74b
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-06 16:22:27 -0300

    fix(back): correção e remoção de createdat e updatedat

--- back/src/database/migrations/20221115160334-Friendship.js
+++ back/src/database/migrations/20221115160334-Friendship.js
@@ -2,43 +2,30 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Friendship', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id_1: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id'
+    await queryInterface.createTable(
+      'Friendships',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: 'cascade'
-      },
-      user_id_2: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id'
+        user_id_1: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onDelete: 'cascade',
         },
-        onDelete: 'cascade'
+        user_id_2: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onDelete: 'cascade',
+        },
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Friendship');
-  }
+    await queryInterface.dropTable('Friendships');
+  },
 };
