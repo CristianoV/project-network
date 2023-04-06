ba82c99bb9e4c6f8b38e6cf4caf818bf82fb0df3
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-06 16:29:22 -0300

    feat(back): implementa novas colunas no model de user

--- back/src/database/models/user.ts
+++ back/src/database/models/user.ts
@@ -1,31 +1,55 @@
-import {  INTEGER, Model, STRING } from 'sequelize';
+import { INTEGER, STRING, TEXT, Model } from "sequelize";
 import db from '.';
 
 class User extends Model {
   public id: number;
   public email: string;
   public password: string;
+  public firstName: string;
+  public lastName: string;
+  public bio: string;
+  public profile_picture: string;
 }
 
-User.init({
-  id: {
-    type: INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: TEXT,
+      allowNull: true,
+    },
+    profile_picture: {
+      type: STRING,
+      allowNull: true,
+    },
   },
-  email: {
-    type: STRING,
-    allowNull: false,
-  },
-  password: {
-    type: STRING,
-    allowNull: false,
+  {
+    sequelize: db,
+    modelName: 'User',
+    timestamps: false,
   }
-}, {
-  sequelize: db,
-  modelName: 'Users',
-  timestamps: false,
-});
+);
 
 export default User;
