7f75dd05b35f7234f009e8b82bcfaf29bf34eab3
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-06 16:25:18 -0300

    feat(back): adiciona interfaces de friendship

---
+++ back/src/interface/IController/IFriendshipController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+export default interface IFriendshipController {
+  getFriends(req: Request, res: Response): Promise<Response>;
+  addFriend(req: Request, res: Response): Promise<Response>;
+  deleteFriend(req: Request, res: Response): Promise<Response>;
+}
