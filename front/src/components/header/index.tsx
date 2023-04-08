ceda60be341bd22cca36c0da555e2edfa3d2362b
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-07 21:15:42 -0300

    feat(front): adiciona links ao header

--- front/src/components/header/index.tsx
+++ front/src/components/header/index.tsx
@@ -8,62 +8,52 @@ import { useRouter } from 'next/router';
 export default function Header() {
   const router = useRouter();
 
-    const logout = async (e: React.FormEvent) => {
-      e.preventDefault();
-  
-      await fetch('/api/logout', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-      });
-      router.push('/');
-    };
+  const logout = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await fetch('/api/logout', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+    router.push('/');
+  };
 
   return (
     <header className={styles.headerContainer}>
-      <nav className={styles.profile}>
-      <Image src={logo} alt='logo' />
-        <ul>
-          <li>
-            <Link href="/">
-              Ínicio
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              Perfil
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              Página de recados
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              Amigos
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              Comunidades
-            </Link>
-          </li>
+      <div>
+        <nav className={styles.profile}>
+          <Image src={logo} alt='logo' />
+          <ul>
+            <li>
+              <Link href='/'>Ínicio</Link>
+            </li>
+            <li>
+              <Link href='/'>Perfil</Link>
+            </li>
+            <li>
+              <Link href='/'>Página de recados</Link>
+            </li>
+            <li>
+              <Link href='/'>Amigos</Link>
+            </li>
+            <li>
+              <Link href='/'>Comunidades</Link>
+            </li>
           </ul>
-      </nav>
-      <div className={styles.profile}>
-      <button onClick={(e) => logout(e)}>
-            Sair
-          </button>
-      <form action="">
-      <input type="text"  placeholder="Pesquisar no Orkut" />
-      <button type="submit">
-        <FaSearch size={20} />
-      </button>
-      </form>
-
+        </nav>
+        <article className={styles.profile}>
+          <button onClick={(e) => logout(e)}>Sair</button>
+          <form action=''>
+            <input type='text' placeholder='Pesquisar no Orkut' />
+            <button type='submit'>
+              <FaSearch size={20} />
+            </button>
+          </form>
+        </article>
       </div>
     </header>
-  )};
+  );
+}
