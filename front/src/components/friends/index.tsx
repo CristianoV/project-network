a4b94678e3751f06ddff19ddf30d091074a2e184
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-07 21:07:56 -0300

    feat(front): adiciona componente de amigos

---
+++ front/src/components/friends/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function Friends() {
+  return (
+    <div className={styles.container}>
+      <h2>meus amigos <span>(0)</span></h2>
+    </div>
+  );
+}
