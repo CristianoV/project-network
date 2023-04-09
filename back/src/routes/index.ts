08a9c921d55eb3918565e364f6e9e0870531668b
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-09 17:02:49 -0300

    feat(back): adiciona rota de partner

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -2,5 +2,12 @@ import RegisterRoutes from './register.router';
 import LoginRoutes from './login.router';
 import UserRoutes from './user.router';
 import FriendRoutes from './friend.router';
+import PartnerRoutes from './partner.router';
 
-export default [RegisterRoutes, LoginRoutes, UserRoutes, FriendRoutes];
+export default [
+  RegisterRoutes,
+  LoginRoutes,
+  UserRoutes,
+  FriendRoutes,
+  PartnerRoutes,
+];
