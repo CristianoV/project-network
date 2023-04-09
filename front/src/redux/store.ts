8a9e27772b428f47aaa807a506c6247253984f86
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-09 16:55:54 -0300

    feat(front): adiciona mais reducers ao projeto

--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import user from './slices/user';
+import friends from './slices/friends';
+import groups from './slices/groups';
 
 const store = configureStore({
   reducer: {
     user,
+    friends,
+    groups,
   },
 });
 
