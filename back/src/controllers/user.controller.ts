228ef7ee067aae4193f2444b658a7d68e82369a8
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-11 09:58:12 -0300

    feat(back): implementa novos metodos no controller de user

--- back/src/controllers/user.controller.ts
+++ back/src/controllers/user.controller.ts
@@ -12,4 +12,29 @@ export default class UserController implements IUserController {
 
     return res.status(200).json(user);
   }
+
+  public async updatePhrase(req: Request, res: Response) {
+    const { authorization } = req.headers as { authorization: string };
+    const { phrase } = req.body as { phrase: string };
+
+    await this.userService.updatePhrase(authorization, phrase);
+
+    return res.status(200).json('Phrase updated');
+  }
+
+  public async updateProfile(req: Request, res: Response) {
+    const { authorization } = req.headers as { authorization: string };
+
+    await this.userService.updateProfile(authorization, req.body);
+
+    return res.status(200).json('Profile updated');
+  }
+
+  public async getUsersByName(req: Request, res: Response) {
+    const { name } = req.params as { name: string };
+
+    const users = await this.userService.getUsersByName(name);
+
+    return res.status(200).json(users);
+  }
 }
