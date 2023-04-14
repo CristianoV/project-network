307448758fde8a39da491942e2a727ed03d631fa
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-14 10:23:37 -0300

    feat(front): adiciona componente de renderização de amigos

---
+++ front/src/components/friendCommunity/index.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.scss';
+import { useSelector } from 'react-redux';
+import Card from './cards';
+import Link from 'next/link';
+
+export default function Friends() {
+  const redux = useSelector((state: any) => state.profile);
+  const groups = redux.groups;
+
+  return (
+    <div className={styles.container}>
+      <h2>
+        minhas comunidades <span>({groups.length})</span>
+      </h2>
+      <div className={styles.cards}>
+        {groups.length > 0 &&
+          groups.map(({ group }: any, index: number) => {
+            if (index < 6) {
+              return <Card key={group.id} group={group} />;
+            }
+          })}
+      </div>
+      <hr />
+      <Link href='/communities' className={styles.link}>
+        ver todos
+      </Link>
+    </div>
+  );
+}
