9f87cecb83a328981331567fd524a901446260ea
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-14 10:31:01 -0300

    feat(front): adiciona componente friendLeftSideBar

---
+++ front/src/components/friendLeftSideBar/index.tsx
@@ -0,0 +1,58 @@
+import styles from './style.module.scss';
+import avatar from '../../../public/avatar.jpg';
+import Image from 'next/image';
+import Link from 'next/link';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineCamera, AiOutlineVideoCamera } from 'react-icons/ai';
+import { BiMessageSquareEdit } from 'react-icons/bi';
+import { MdRssFeed } from 'react-icons/md';
+import { IoIosList } from 'react-icons/io';
+import { BsEnvelope } from 'react-icons/bs';
+import { CiSettings } from 'react-icons/ci';
+import { GiSunglasses } from 'react-icons/gi';
+import { useSelector } from 'react-redux';
+
+export default function LeftSideBar() {
+  const redux = useSelector((state: any) => state.profile);
+
+  const { info } = redux;
+  return (
+    <div className={styles.container}>
+      <Image src={avatar} alt='avatar' width={500} height={500} />
+      <hr />
+      <div>
+        <h2>
+          {info?.firstName} {info?.lastName}
+        </h2>
+        {info?.birthday && <p>{info?.sex}</p>}
+        {info.relationship && <p>{info?.relationship}</p>}
+        {info?.country && <p>{info?.country}</p>}
+      </div>
+      <hr />
+      <div>
+        <Link href='/profile/edit'>
+          <CgProfile /> + amigo
+        </Link>
+      </div>
+      <hr />
+      <div>
+        <Link href='/profile'>
+          <CgProfile /> perfil
+        </Link>
+        <Link href='/'>
+          <BiMessageSquareEdit /> recados
+        </Link>
+        <Link href='/'>
+          <AiOutlineCamera /> álbuns
+        </Link>
+        <Link href='/'>
+          <AiOutlineVideoCamera /> vídeos
+        </Link>
+        <Link href='/'>
+          <GiSunglasses /> depoimentos
+        </Link>
+      </div>
+      <hr />
+    </div>
+  );
+}
