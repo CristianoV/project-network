68e4fb94b7812090fc07975cafe3441bf7cec89c
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-15 19:30:07 -0300

    feat(front): adiciona função de upload de imagens

--- front/src/components/leftSideBar/index.tsx
+++ front/src/components/leftSideBar/index.tsx
@@ -11,14 +11,66 @@ import { BsEnvelope } from 'react-icons/bs';
 import { CiSettings } from 'react-icons/ci';
 import { GiSunglasses } from 'react-icons/gi';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { fetchFromApi } from '../../utils/axios';
 
-export default function LeftSideBar() {
+interface LeftSideBarProps {
+  token: string;
+}
+
+export default function LeftSideBar({ token }: LeftSideBarProps) {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]);
+    }
+  };
+
+  const handleUploadClick = async () => {
+    try {
+      const headers = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: token,
+        },
+      };
+      await fetchFromApi.patch(`/user/image`, { foto: selectedFile }, headers);
+
+      router.push('/profile');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const router = useRouter();
   const redux = useSelector((state: any) => state.user);
 
   const { info } = redux;
   return (
     <div className={styles.container}>
-      <Image src={info?.profile_picture || avatar} alt='avatar' width={500} height={500} />
+      <Image
+        src={
+          (selectedFile
+            ? URL.createObjectURL(selectedFile)
+            : info?.profile_picture) || avatar
+        }
+        alt='avatar'
+        width={500}
+        height={500}
+      />
+      {router.pathname === '/profile/edit' && (
+        <div className={styles.updateImage}>
+          <p>Alterar foto de perfil</p>
+          <input type='file' onChange={handleFileInputChange} />
+          <button onClick={handleUploadClick} disabled={!selectedFile}>
+            Enviar
+          </button>
+        </div>
+      )}
       <hr />
       <div>
         <h2>
