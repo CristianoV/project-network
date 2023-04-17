ac9296e6afefeb159e5045503ed15bc4a608db1f
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-17 13:56:25 -0300

    feat(back): adicionanova coluna na tabela de Friendships

--- back/src/database/migrations/20221115160334-Friendship.js
+++ back/src/database/migrations/20221115160334-Friendship.js
@@ -2,31 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      'Friendships',
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        user_id_1: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: { model: 'Users', key: 'id' },
-          allowNull: false,
-          onDelete: 'cascade',
-        },
-        user_id_2: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: { model: 'Users', key: 'id' },
-          allowNull: false,
-          onDelete: 'cascade',
-        },
+    await queryInterface.createTable('Friendships', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
       },
-    );
+      user_id_1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' },
+        allowNull: false,
+        onDelete: 'cascade',
+      },
+      user_id_2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' },
+        allowNull: false,
+        onDelete: 'cascade',
+      },
+      status: {
+        type: Sequelize.ENUM('pending', 'accepted', 'rejected'),
+        allowNull: false,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
