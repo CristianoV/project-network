eaf58a4a5d079777e58efb9368522f41a208786a
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-20 16:52:20 -0300

    feat(back): adiciona novas interfaces no projeto

--- back/src/interface/IService/IPartnerService.ts
+++ back/src/interface/IService/IPartnerService.ts
@@ -1,4 +1,18 @@
 export default interface IPartnerService<T> {
   getPartners(authorization: string): Promise<T[]>;
   getPartnersByUserId(id: number): Promise<T[]>;
+  createPartner({
+    authorization,
+    groupId,
+  }: {
+    authorization: string;
+    groupId: number;
+  }): Promise<T>;
+  isPartner({
+    authorization,
+    groupId,
+  }: {
+    authorization: string;
+    groupId: number;
+  }): Promise<boolean>;
 }
