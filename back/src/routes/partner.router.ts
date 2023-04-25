c93c6eb02ba7513277ccf5c04906096ead1aed7e
Cristiano Vieira <cristianoviieira@gmail.com>
2023-04-24 23:29:54 -0300

    fix(back): altera nomes de constantes

--- back/src/routes/partner.router.ts
+++ back/src/routes/partner.router.ts
@@ -1,30 +1,34 @@
 import { Request, Response, Router } from 'express';
-import FriendController from '../controllers/partner.controller';
-import FriendService from '../services/partner.service';
-import FriendModel from '../database/models/partners';
+import PartnersModelController from '../controllers/partner.controller';
+import PartnersModelService from '../services/partner.service';
+import PartnersModelModel from '../database/models/partners';
 
-const FriendRoutes: Router = Router();
-const friendService = new FriendService(FriendModel);
-const friendController = new FriendController(friendService);
+const PartnersRoutes: Router = Router();
+const partnerService = new PartnersModelService(PartnersModelModel);
+const partnerController = new PartnersModelController(partnerService);
 
-FriendRoutes.get('/partner', (request: Request, response: Response) =>
-  friendController.getPartners(request, response)
+PartnersRoutes.get('/partner', (request: Request, response: Response) =>
+  partnerController.getPartners(request, response)
 );
 
-FriendRoutes.get('/partner/:id', (request: Request, response: Response) =>
-  friendController.getPartnersByUserId(request, response)
+PartnersRoutes.get('/partner/:id', (request: Request, response: Response) =>
+  partnerController.getPartnersByUserId(request, response)
 );
 
-FriendRoutes.get('/partner/group/:id', (request: Request, response: Response) =>
-  friendController.getPartnerByGroupId(request, response)
+PartnersRoutes.get(
+  '/partner/group/:id',
+  (request: Request, response: Response) =>
+    partnerController.getPartnerByGroupId(request, response)
 );
 
-FriendRoutes.post('/partner', (request: Request, response: Response) =>
-  friendController.createPartner(request, response)
+PartnersRoutes.post('/partner', (request: Request, response: Response) =>
+  partnerController.createPartner(request, response)
 );
 
-FriendRoutes.get('/partner/is/:groupId', (request: Request, response: Response) =>
-  friendController.isPartner(request, response)
+PartnersRoutes.get(
+  '/partner/is/:groupId',
+  (request: Request, response: Response) =>
+    partnerController.isPartner(request, response)
 );
 
-export default FriendRoutes;
+export default PartnersRoutes;
