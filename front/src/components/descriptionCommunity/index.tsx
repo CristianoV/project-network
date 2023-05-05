5233925f07e3329409122d946c8a5163fc9d81c8
Cristiano Vieira <cristianoviieira@gmail.com>
2023-05-04 21:55:52 -0300

    feat(front): adiciona renderização condicional de comunidades

--- front/src/components/descriptionCommunity/index.tsx
+++ front/src/components/descriptionCommunity/index.tsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
 export default function DescriptionCommunity() {
+  const redux = useSelector((state: any) => state.user);
+  const { groups } = redux;
+
   return (
     <div className={styles.container}>
       <h2>Comunidades</h2>
@@ -9,15 +13,42 @@ export default function DescriptionCommunity() {
         <Link href='/'>Ínicio</Link>
         <p>{'>'} Comunidades</p>
       </nav>
-      <div className={styles.text}>
-        <p>
-          Compartilhe suas paixões. Conheça pessoas que tem interesses em comum.
-          Troque idéias, Planeje eventos...
-        </p>
-        <p>
-          Você pode pesquisar comunidades ou criar a sua própria comunidade.
-        </p>
-      </div>
+      {groups.length === 0 && (
+        <div className={styles.text}>
+          <p>
+            Compartilhe suas paixões. Conheça pessoas que tem interesses em
+            comum. Troque idéias, Planeje eventos...
+          </p>
+          <p>
+            Você pode pesquisar comunidades ou criar a sua própria comunidade.
+          </p>
+        </div>
+      )}
+      {groups.length > 0 && (
+        <div className={styles.groups}>
+          <nav>
+            <h2 className={styles.selected}>Todos ({groups.length})</h2>
+            <h2>Comunidades que sou dono</h2>
+            <h2>Pendentes</h2>
+          </nav>
+          <hr />
+          <div>
+            {groups.map(({ group }: any, index: number) => {
+              return (
+                <Link
+                  href={`/community/${group.id}`}
+                  key={index}
+                  className={styles.cards}
+                >
+                  <div key={group.id}>
+                    <p>{group.name}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
