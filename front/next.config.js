1fada3f6ab5683cb28b816dbcbc9bdd0f5bb9650
Cristiano Vieira <cristianoviieira@gmail.com>
2023-07-09 02:20:32 -0300

    feat: add new 2 games in project

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'backend', 'project-network-production.up.railway.app'],
+    domains: [
+      'localhost',
+      'backend',
+      'project-network-production.up.railway.app',
+      'bucket-orkut.s3.amazonaws.com',
+      'bucket-orkut.s3-sa-east-1.amazonaws.com',
+      'bucket-orkut.s3.us-east-2.amazonaws.com',
+      'bucket-orkut.s3.us-east-2.amazonaws.com/',
+      'bucket-orkut.s3.us-east-3.amazonaws.com',
+      'https://bucket-orkut.s3.us-east-2.amazonaws.com/',
+      'https://bucket-orkut.s3.us-east-2.amazonaws.com',
+    ],
   },
 };
 
