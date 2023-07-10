df96ed5dc61c74ad41bcbe5181f0fc40f28c4eae
Cristiano Vieira <cristianoviieira@gmail.com>
2023-07-10 18:02:35 -0300

    feat: add new routes to edit and delete posts

--- back/src/services/post.service.ts
+++ back/src/services/post.service.ts
@@ -26,6 +26,38 @@ export default class PostsService {
     return post;
   }
 
+  public async updatePost({
+    id,
+    image,
+    text,
+    authorization,
+  }: {
+    image: string | null;
+    text: string | null;
+    authorization: string;
+    id: number;
+  }) {
+    const { id: userId } = JwtSecret.verify(authorization);
+
+    const post = await this.model.findOne({
+      where: {
+        id,
+        user_id: userId,
+      },
+    });
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    if (image) post.image = image;
+    if (text) post.text = text;
+
+    await post.save();
+
+    return post;
+  }
+
   public async getPosts({
     authorization,
     page,
@@ -36,14 +68,14 @@ export default class PostsService {
     pageSize: number;
   }) {
     const { id } = JwtSecret.verify(authorization);
-  
+
     const friends = await Friends.findAll({
       where: {
         [Op.or]: [{ user_id_1: id }, { user_id_2: id }],
         status: 'accepted',
       },
     });
-  
+
     const friendIds = friends.flatMap((item) => {
       if (item.user_id_1 === id) {
         return item.user_id_2;
@@ -51,9 +83,9 @@ export default class PostsService {
         return item.user_id_1;
       }
     }) as number[];
-  
+
     friendIds.push(Number(id));
-  
+
     const posts = await this.model.findAll({
       order: [['created_at', 'DESC']],
       where: {
@@ -65,12 +97,36 @@ export default class PostsService {
         },
       ],
     });
-  
+
     const startIndex = (page - 1) * pageSize;
     const endIndex = page * pageSize;
     const result = posts.slice(startIndex, endIndex);
-  
+
     return result;
   }
-  
+
+  public async deletePost({
+    authorization,
+    id,
+  }: {
+    authorization: string;
+    id: number;
+  }) {
+    const { id: userId } = JwtSecret.verify(authorization);
+
+    const post = await this.model.findOne({
+      where: {
+        id,
+        user_id: userId,
+      },
+    });
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    await post.destroy();
+
+    return post;
+  }
 }
