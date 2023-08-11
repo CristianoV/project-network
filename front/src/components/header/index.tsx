ddcfa3d6c6ca8d6f3e77d6b4df93ceeab72df5ac
Cristiano Vieira <cristianoviieira@gmail.com>
2023-08-10 22:57:51 -0300

    style: port mobile

--- front/src/components/header/index.tsx
+++ front/src/components/header/index.tsx
@@ -2,10 +2,17 @@ import Link from 'next/link';
 import styles from './styles.module.scss';
 import logo from '../../../public/orkutLogo.png';
 import Image from 'next/image';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaUserFriends } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { FormEvent, useCallback, useEffect, useState } from 'react';
+import {
+  AiOutlineHome,
+  AiOutlineMessage,
+  AiOutlineProfile,
+} from 'react-icons/ai';
+import { RiCommunityLine } from 'react-icons/ri';
+import { LiaUserFriendsSolid } from 'react-icons/lia';
 
 export default function Header() {
   const [search, setSearch] = useState('');
@@ -38,47 +45,85 @@ export default function Header() {
   }, [loading, logout]);
 
   return (
-    <header className={styles.headerContainer}>
-      <div>
+    <>
+      <header className={styles.headerContainer}>
+        <div>
+          <nav className={styles.profile}>
+            <Image src={logo} alt='logo' />
+            <ul>
+              <li>
+                <Link href='/'>Ínicio</Link>
+              </li>
+              <li>
+                <Link href='/profile'>Perfil</Link>
+              </li>
+              <li>
+                <Link href='/messages'>Página de recados</Link>
+              </li>
+              <li>
+                <Link href='/friends'>Amigos</Link>
+              </li>
+              <li>
+                <Link href='/community'>Comunidades</Link>
+              </li>
+            </ul>
+          </nav>
+          <article className={styles.profile}>
+            <p className={styles.mail}>{info.email}</p>
+            <button onClick={() => logout()}>Sair</button>
+            {!router.pathname.startsWith(searchPath) && (
+              <form onSubmit={handleSearch}>
+                <input
+                  type='text'
+                  placeholder='Pesquisar no Orkut'
+                  value={search}
+                  required
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button type='submit'>
+                  <FaSearch size={20} />
+                </button>
+              </form>
+            )}
+          </article>
+        </div>
+      </header>
+      <div className={styles.header_mobile}>
         <nav className={styles.profile}>
-          <Image src={logo} alt='logo' />
           <ul>
             <li>
-              <Link href='/'>Ínicio</Link>
+              <Link href='/'>
+                <AiOutlineHome size={20} />
+                Ínicio
+              </Link>
             </li>
             <li>
-              <Link href='/profile'>Perfil</Link>
+              <Link href='/profile'>
+                <AiOutlineProfile size={20} />
+                Perfil
+              </Link>
             </li>
             <li>
-              <Link href='/messages'>Página de recados</Link>
+              <Link href='/messages'>
+                <AiOutlineMessage size={20} />
+                recados
+              </Link>
             </li>
             <li>
-              <Link href='/friends'>Amigos</Link>
+              <Link href='/friends'>
+                <LiaUserFriendsSolid size={20} />
+                Amigos
+              </Link>
             </li>
             <li>
-              <Link href='/community'>Comunidades</Link>
+              <Link href='/community'>
+                <RiCommunityLine size={20} />
+                Comunidades
+              </Link>
             </li>
           </ul>
         </nav>
-        <article className={styles.profile}>
-          <p>{info.email}</p>
-          <button onClick={() => logout()}>Sair</button>
-          {!router.pathname.startsWith(searchPath) && (
-            <form onSubmit={handleSearch}>
-              <input
-                type='text'
-                placeholder='Pesquisar no Orkut'
-                value={search}
-                required
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <button type='submit'>
-                <FaSearch size={20} />
-              </button>
-            </form>
-          )}
-        </article>
       </div>
-    </header>
+    </>
   );
 }
